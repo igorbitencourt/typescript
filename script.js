49af56b64b3b01d1dc0f2f4ca4e410fda40797fc
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-11-30 10:53:49 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 "use strict";
-async function fetchCursos() {
-    const response = await fetch("https://api.origamid.dev/json/cursos.json");
-    const data = await response.json();
-    showCursos(data);
-}
-fetchCursos();
-function showCursos(cursos) {
-    cursos.forEach((curso) => {
-        let color;
-        if (curso.nivel === "iniciante") {
-            color = "blue";
-        }
-        else if (curso.nivel === "avancado") {
-            color = "red";
-        }
-        document.body.innerHTML += `
-      <div>
-      <h2 style="color: ${color}">Curso: ${curso.nome}<h2>
-      <p>Horas: ${curso.horas}</p>
-      <p>Aulas: ${curso.aulas}</p>
-      <p>Tipo: ${curso.gratuito ? "Gratuito" : "Pago"}</p>
-      <p>tags: ${curso.tags.join(", ")}</p>
-      <p>Aulas: ${curso.idAulas.join(" | ")}</p>
-
-      </div>
-    `;
-    });
+const link = document.getElementById("origamid");
+if (link instanceof HTMLAnchorElement) {
+    link.href = link.href.replace("http", "https");
 }
