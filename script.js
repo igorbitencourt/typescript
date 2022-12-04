f1b047d6853305498036fdf7a9d4258d0ca3c7de
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-12-04 08:30:08 -0300

    exercicio 9

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 "use strict";
-// Estado dos elementos
-// menu inativo:
-// class="" em nav
-// aria-expanded="false" em button
-// aria-label="Abrir Menu" em button
-// menu ativo:
-// class="active" em nav
-// aria-expanded="true" em button
-// aria-label="Fechar Menu" em button
-const botao = document.getElementById("btn-mobile");
-function ativarMenu(elemento) {
-    const menu = document.getElementById("nav");
-    const button = elemento.currentTarget;
-    if (button instanceof HTMLElement && menu) {
-        menu?.classList.toggle("active");
-        const active = menu?.classList.contains("active");
-        if (active) {
-            button.setAttribute("aria-expended", "false");
-            button.setAttribute("aria-label", "Abrir Menu");
-        }
-        else {
-            button.setAttribute("aria-expended", "true");
-            button.setAttribute("aria-label", "Fechar Menu");
-        }
+function arredondar(n) {
+    if (typeof n === "number") {
+        return Math.ceil(n);
+    }
+    else {
+        return Math.ceil(+n).toString();
     }
 }
-botao?.addEventListener("pointerdown", ativarMenu);
+console.log(arredondar(2.43));
+console.log(arredondar("2.43"));
